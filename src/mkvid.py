004cf57b7ac08690378deeecd1e4e2cb6c6405c6
Robert Vava <77018483+robertvava@users.noreply.github.com>
2023-04-16 15:57:14 +0100

    Update mkvid.py

--- src/mkvid.py
+++ src/mkvid.py
@@ -1,24 +1,4 @@
-# import os
-# import cv2 as cv
 
-# def mkvid(target_dir = '../frames', endswith = "jpg"):
-#     videodims = (640,480)
-#     fps = 24  # frames per second
-
-#     # Create a list of the frames filenames.
-#     images = [img for img in os.listdir(target_dir) if img.endswith(".jpg")] 
-#     images.sort(key=lambda x: (int(x.split(None, 1)[0]) if x[-4:].isdigit() else 999, x)) #  Sort the names in the list for joining the frames consecutively.  
-#     frame = cv.imread(os.path.join(target_dir, images[0])) # Initialize first frame. 
-#     height, width, layers = frame.shape 
-#     video = cv.VideoWriter('demo.avi',cv.VideoWriter_fourcc(*'XVID'), fps, (width,height))
-
-
-#     for image in images:
-#         video.write(cv.imread(os.path.join(target_dir, image)))
-
-
-#     cv.destroyAllWindows()
-#     video.release()
 
 import os
 import cv2 as cv
